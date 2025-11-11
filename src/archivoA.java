d89ead62f8efd46d1378458680ced55b270e3944
GusSanViator <gustavojavieer.varmel@sanviatorvalladolid.com>
2025-11-11 09:42:41 +0100

    subir archivos

--- src/archivoA.java
+++ src/archivoA.java
@@ -3,11 +3,11 @@ public class archivoA {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		funcion1();
 	}
 	
 	public static void funcion1()
 	{
-		System.out.println("hola, esta es la funcion 1");
+		System.out.println("buenos dias, esta es la funcion 1");
 	}
 }
