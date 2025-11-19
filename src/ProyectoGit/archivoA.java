99b9edfe6e039b85a531abb98cf4e2e3b280ae2a
GusSanViator <gustavojavieer.varmel@sanviatorvalladolid.com>
2025-11-19 14:02:42 +0100

    ejercicio 1 añadido

---
+++ src/ProyectoGit/archivoA.java
@@ -0,0 +1,14 @@
+package ProyectoGit;
+
+public class archivoA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ejercicio1();
+	}
+
+	public static void ejercicio1()
+	{
+		System.out.println("este es el ejercicio 1");
+	}
+}
