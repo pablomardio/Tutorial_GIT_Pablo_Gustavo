c45d1b678cbabf38c57f777eb19f8ed04ad7c001
pablomardio <pablo.mardio@sanviatorvalladolid.com>
2025-11-20 09:49:44 +0100

    He creado la funcion 3

--- src/ProyectoGit/archivoB.java
+++ src/ProyectoGit/archivoB.java
@@ -6,12 +6,15 @@ public class archivoB {
 		// TODO Auto-generated method stub
 		funcion1();
 		funcion2();
-
+		funcion3();
 	}
 	public static void funcion1() {
 		System.out.println("Esta es la funcion 1");
 	}
 	public static void funcion2() {
 		System.out.println("Esta es la funcion 2");
+	}
+	public static void funcion3() {
+		System.out.println("Esta es la funcion 3");
 	}
 }
