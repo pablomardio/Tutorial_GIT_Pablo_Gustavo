d944ac31c6057794d99305a15af7502bd8f8c275
pablomardio <pablo.mardio@sanviatorvalladolid.com>
2025-11-20 17:42:08 +0100

    Experimental 2

--- src/ProyectoGit/archivoD.java
+++ src/ProyectoGit/archivoD.java
@@ -5,8 +5,12 @@ public class archivoD {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		experimentalB1();
+		experimentalB2();
 	}
 	public static void experimentalB1() {
 		System.out.println("Experimental 1");
+	}
+	public static void experimentalB2() {
+		System.out.println("Experimental 2");
 	}
 }
