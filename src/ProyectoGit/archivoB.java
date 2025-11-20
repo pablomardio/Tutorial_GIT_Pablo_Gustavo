3dbc56dfbdf3cce93b9ee9c86405773877dfc422
pablomardio <pablo.mardio@sanviatorvalladolid.com>
2025-11-20 09:54:12 +0100

    He creado la funcion 5

--- src/ProyectoGit/archivoB.java
+++ src/ProyectoGit/archivoB.java
@@ -8,6 +8,7 @@ public class archivoB {
 		funcion2();
 		funcion3();
 		funcion4();
+		funcion5();
 	}
 	public static void funcion1() {
 		System.out.println("Esta es la funcion 1");
@@ -20,5 +21,8 @@ public class archivoB {
 	}
 	public static void funcion4() {
 		System.out.println("Esta es la funcion 4");
+	}
+	public static void funcion5() {
+		System.out.println("Esta es la funcion 5");
 	}
 }
