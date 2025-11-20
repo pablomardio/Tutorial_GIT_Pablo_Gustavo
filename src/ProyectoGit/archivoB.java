95fbab707b79cda150aead7bddec578ed6103126
pablomardio <pablo.mardio@sanviatorvalladolid.com>
2025-11-20 09:45:34 +0100

    He creado la funcion 1

---
+++ src/ProyectoGit/archivoB.java
@@ -0,0 +1,14 @@
+package ProyectoGit;
+
+public class archivoB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		funcion1();
+
+	}
+	public static void funcion1() {
+		System.out.println("Esta es la funcion 1");
+	}
+
+}
