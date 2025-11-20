64c7a2e9cad6832debb4262964f90cb8d164b04d
GusSanViator <gustavojavieer.varmel@sanviatorvalladolid.com>
2025-11-20 11:49:42 +0100

    Creao ArchivoC y experimental4

---
+++ src/ProyectoGit/ArchivoC.java
@@ -0,0 +1,15 @@
+package ProyectoGit;
+
+public class ArchivoC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static void experimental4()
+	{
+		System.out.println("adios");
+	}
+
+}
