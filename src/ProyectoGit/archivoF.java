71ad25261c31b6a6017227b1e41bc1c53627ebae
pablomardio <pablo.mardio@sanviatorvalladolid.com>
2025-11-21 12:55:00 +0100

    Esta es la funcionF1

---
+++ src/ProyectoGit/archivoF.java
@@ -0,0 +1,11 @@
+package ProyectoGit;
+
+public class archivoF {
+
+	public static void main(String[] args) {
+		funcionF1();
+	}
+	public static void funcionF1(){
+		System.out.println("Esta es la funcionF1");
+	}
+}
