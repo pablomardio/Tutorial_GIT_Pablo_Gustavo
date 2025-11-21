aea819f94721d19e794b9d86727ed9bf29985b49
pablomardio <pablo.mardio@sanviatorvalladolid.com>
2025-11-21 12:56:12 +0100

    Esta es la funcionF2

--- src/ProyectoGit/archivoF.java
+++ src/ProyectoGit/archivoF.java
@@ -4,8 +4,12 @@ public class archivoF {
 
 	public static void main(String[] args) {
 		funcionF1();
+		funcionF2();
 	}
 	public static void funcionF1(){
 		System.out.println("Esta es la funcionF1");
+	}
+	public static void funcionF2() {
+		System.out.println("Esta es la funcionF2");
 	}
 }
